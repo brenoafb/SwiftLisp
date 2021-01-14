35ca056fd59a2a413f2f5db909765835de4f186a
brenoafb <breno.afb@gmail.com>
2021-01-14 02:57:50 -0300

    Remove unused input code

--- Sources/SwiftLisp/main.swift
+++ Sources/SwiftLisp/main.swift
@@ -8,63 +8,6 @@ enum InputValidation {
   case invalid
 }
 
-func checkParens(_ str: String) -> InputValidation {
-  var count = 0
-  for char in str {
-    switch char {
-    case "(":
-      count += 1
-    case ")":
-      if count == 0 {
-        return .invalid
-      }
-      count -= 1
-    default:
-      break
-    }
-    
-  }
-  return count == 0 ? .valid : .incomplete
-}
-
-//func readInput(_ input: String = "") -> String {
-//  var currInput = input
-//  guard let line = readLine() else {
-//    return ""
-//  }
-//
-//  currInput += line
-//  switch checkParens(currInput) {
-//  case .incomplete:
-//    print("~", separator: "", terminator: " ")
-//    return readInput(currInput)
-//  case .invalid:
-//    print("Invalid input")
-//    return readInput()
-//  case .valid:
-//    return currInput
-//  }
-//}
-//
-//func repl() {
-//  print(">", separator: "", terminator: " ")
-//  let input = readInput()
-//  if let exprs = Parser.parse(input) {
-//    for expr in exprs {
-//      if let result = expr.eval(env) {
-//        print("> \(result)")
-//      } else {
-//        print("ERROR")
-//      }
-//    }
-//  } else {
-//    print("Parsing error")
-//  }
-//  repl()
-//}
-
-// repl()
-
 func readInput() -> String {
   var input = ""
   while let line = readLine(strippingNewline: true) {
@@ -78,22 +21,22 @@ func execFile(filename: String, env: Environment) -> Bool {
     print("Error reading file \(filename)")
     return false
   }
-  
+
   guard let exprs = Parser.parse(contents) else {
     print("Error parsing file \(filename)")
     return false
   }
-  
+
   for expr in exprs {
     print(expr)
     guard let result = expr.eval(env) else {
       print("Error evaluatng expression in file \(filename)")
       return false
     }
-    
+
     print(result)
   }
-  
+
   return true
 }
 
@@ -102,20 +45,3 @@ if CommandLine.argc > 1 {
     let _ = execFile(filename: argument, env: env)
   }
 }
-
-//let input = readInput()
-//if let exprs = Parser.parse(input) {
-//  for expr in exprs {
-//    print(expr)
-//  }
-//  for expr in exprs {
-//    if let result = expr.eval(env) {
-//      print(result)
-//    } else {
-//      print("Eval error")
-//    }
-//  }
-//  
-//} else {
-//  print("Parse error")
-//}
