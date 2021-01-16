5cb0b67e8c1fbb13f48a7df0625118803ea198c5
brenoafb <breno.afb@gmail.com>
2021-01-16 19:37:58 -0300

    Add facilities to convert swift values to lisp exprs

--- Sources/SwiftLisp/env.swift
+++ Sources/SwiftLisp/env.swift
@@ -66,8 +66,82 @@ var defaultEnvironment = Environment([[
   "!=.f": .native(binaryFloatComparison({ $0 != $1 })),
   "println": .native(printlnFunc),
   "print": .native(printFunc),
-  "request": .native(request)
+  "request": .native(request),
+  "json-to-assoc-list": .native(jsonToAssocList),
 ]])
+
+func valueToExpr(_ value: Any) -> Expr? {
+  if let value = value as? Int {
+    return .int(value)
+  }
+
+  if let value = value as? Double {
+    return .float(value)
+  }
+
+  if let value = value as? String {
+    return .string(value)
+  }
+
+  if let value = value as? [String:Any] {
+    return dictToAssocList(value)
+  }
+
+  if let value = value as? [Any] {
+    return arrayToList(value)
+  }
+
+  return nil
+}
+
+func arrayToList(_ arr: [Any]) -> Expr? {
+  var list: [Expr] = []
+  for elem in arr {
+    guard let expr = valueToExpr(elem) else {
+      return nil
+    }
+    list.append(expr)
+  }
+
+  return .list(list)
+}
+
+func dictToAssocList(_ dict: [String:Any]) -> Expr? {
+  var list: [Expr] = []
+
+  for (key, value) in dict {
+    guard let expr = valueToExpr(value) else {
+      return nil
+    }
+
+    list.append(.list([.string(key), expr]))
+  }
+
+  return .list(list)
+}
+
+let jsonToAssocList: NativeFunction = { (args) -> Expr? in
+  guard args.count == 1 else {
+    return nil
+  }
+
+  guard case let .string(json) = args.first as? Expr else {
+    return nil
+  }
+
+  let data = Data(json.utf8)
+
+  guard let dict = try? JSONSerialization.jsonObject(with: data, options: []) as? [String:Any] else {
+    print("Error converting data to dictionary")
+    return nil
+  }
+
+  guard let assocList = dictToAssocList(dict) else {
+    return nil
+  }
+
+  return assocList
+}
 
 let request: NativeFunction = { (args) -> Expr? in
   guard args.count == 1 else {
--- http-request.lisp
+++ http-request.lisp
@@ -1,3 +1,17 @@
 (define url "https://jsonplaceholder.typicode.com/todos/1")
 
-(request url)
+(define json (request url))
+
+json
+
+(define dict (json-to-dict json))
+
+dict
+
+(assoc "userId" dict)
+
+(assoc "id" dict)
+
+(assoc "title" dict)
+
+(assoc "completed" dict)
